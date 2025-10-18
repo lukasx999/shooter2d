65cada8528a79a7be9138276540fe738b42f3857
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2025-10-18 18:49:20 +0200

    projectile system

--- main.cc
+++ main.cc
@@ -1,8 +1,58 @@
+#include <vector>
+
 #include <gfx.hh>
 
 // 2d twin stick shooter bullet hell game with procedurally generated levels
 
+namespace {
+
 enum class Direction { North, East, South, West };
+
+class Projectile {
+    gfx::Vec m_position;
+    const gfx::Vec m_direction;
+    const float m_travel_speed;
+
+public:
+    Projectile(gfx::Vec position, gfx::Vec direction, float travel_speed)
+        : m_position(position)
+        , m_direction(direction)
+        , m_travel_speed(travel_speed)
+    { }
+
+    void update(float dt) {
+        m_position += m_direction * m_travel_speed * dt;
+    }
+
+    void draw(gfx::Renderer& rd) const {
+        rd.draw_circle(m_position, 5.0, gfx::Color::green());
+    }
+
+};
+
+class ProjectileSystem {
+    std::vector<Projectile> projectiles;
+
+public:
+    ProjectileSystem() = default;
+
+    void draw(gfx::Renderer& rd) const {
+        for (auto& projectile : projectiles) {
+            projectile.draw(rd);
+        }
+    }
+
+    void update(float dt) {
+        for (auto& projectile : projectiles) {
+            projectile.update(dt);
+        }
+    }
+
+    void add(gfx::Vec position, gfx::Vec direction, float size) {
+        projectiles.emplace_back(Projectile{ position, direction, size });
+    }
+
+};
 
 class Player {
     gfx::Vec m_position;
@@ -47,12 +97,16 @@ public:
 
 };
 
+} // namespace
+
 int main() {
 
     gfx::Window window(1600, 900, "my window", gfx::WindowFlags::DisableVsync);
     gfx::Renderer rd(window);
 
     Player player({window.get_width()/2.0f, window.get_height()/2.0f});
+
+    ProjectileSystem projectiles;
 
     rd.draw([&]() {
         float dt = rd.get_frame_time();
@@ -61,6 +115,19 @@ int main() {
 
         rd.draw_rectangle(0, 0, window.get_width(), window.get_height(), 0_deg, gfx::Color::grey());
         player.draw(rd);
+
+        rd.draw_circle(window.get_mouse_pos(), 15.0, gfx::Color::red());
+        projectiles.draw(rd);
+        projectiles.update(dt);
+
+        if (window.get_mouse_button_state(gfx::MouseButton::LMB).pressed()) {
+            auto ppos = player.get_position();
+            auto mpos = window.get_mouse_pos();
+
+            auto direction = (mpos - ppos).normalized();
+            projectiles.add(ppos, direction, 500.0);
+
+        }
 
         if (window.get_key_state(gfx::Key::W).pressed())
             player.move(Direction::North, dt);
