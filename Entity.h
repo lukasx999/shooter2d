aeffe7c398b14998753c0efe0b0da5084476d1b6
lukasx999 <116069013+lukasx999@users.noreply.github.com>
2026-01-11 03:47:58 +0100

    cleaned up entity

--- Entity.h
+++ Entity.h
@@ -5,31 +5,20 @@
 #include "GameObject.h"
 #include "misc.h"
 
-enum class Direction {
-    North,
-    NorthEast,
-    East,
-    SouthEast,
-    South,
-    SouthWest,
-    West,
-    NorthWest,
-};
-
 class Entity : public IGameObject {
 protected:
-    static constexpr int m_max_health = 100;
-    float m_movement_speed = 500;
     const int m_width;
     const int m_height;
 
+    float m_movement_speed = 500;
     gfx::Vec m_position;
     Direction m_direction = Direction::South;
+
+    static constexpr int m_max_health = 100;
     int m_health = m_max_health;
 
     bool m_is_idle = false;
     bool m_idle_lock = false;
-
     bool m_is_first_iteration = true;
 
 public:
@@ -69,9 +58,10 @@ public:
         m_position = position;
     }
 
-    [[nodiscard]] gfx::Rect get_hitbox() const;
     void update(double dt) override;
     void draw([[maybe_unused]] gfx::Renderer& rd) const override { }
+
+    [[nodiscard]] gfx::Rect get_hitbox() const;
     void walk(Direction dir, double dt);
     void attack();
 
@@ -80,28 +70,6 @@ protected:
     virtual void on_state_change([[maybe_unused]] State new_state) { }
     [[nodiscard]] virtual bool is_attack_done() const = 0;
 
-};
-
-template <>
-struct std::formatter<Direction> : std::formatter<std::string> {
-    auto format(const Direction& direction, std::format_context& ctx) const {
-
-        auto fmt = [&] {
-            switch (direction) {
-                using enum Direction;
-                case North:     return STRINGIFY(North);
-                case East:      return STRINGIFY(East);
-                case South:     return STRINGIFY(South);
-                case West:      return STRINGIFY(West);
-                case NorthEast: return STRINGIFY(NorthEast);
-                case SouthEast: return STRINGIFY(SouthEast);
-                case SouthWest: return STRINGIFY(SouthWest);
-                case NorthWest: return STRINGIFY(NorthWest);
-            }
-        }();
-
-        return std::formatter<std::string>::format(fmt, ctx);
-    }
 };
 
 template <>
--- misc.h
+++ misc.h
@@ -1,5 +1,40 @@
 #pragma once
 
+#include <format>
+
 #include <gfx.h>
 
 #define STRINGIFY(x) (static_cast<void>(x), #x)
+
+enum class Direction {
+    North,
+    NorthEast,
+    East,
+    SouthEast,
+    South,
+    SouthWest,
+    West,
+    NorthWest,
+};
+
+template <>
+struct std::formatter<Direction> : std::formatter<std::string> {
+    auto format(const Direction& direction, std::format_context& ctx) const {
+
+        auto fmt = [&] {
+            switch (direction) {
+                using enum Direction;
+                case North:     return STRINGIFY(North);
+                case East:      return STRINGIFY(East);
+                case South:     return STRINGIFY(South);
+                case West:      return STRINGIFY(West);
+                case NorthEast: return STRINGIFY(NorthEast);
+                case SouthEast: return STRINGIFY(SouthEast);
+                case SouthWest: return STRINGIFY(SouthWest);
+                case NorthWest: return STRINGIFY(NorthWest);
+            }
+        }();
+
+        return std::formatter<std::string>::format(fmt, ctx);
+    }
+};
